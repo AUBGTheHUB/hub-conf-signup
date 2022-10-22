afe060192183ece7eee6d957e145fc5583f7f248
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2022-10-22 13:14:28 +0300

    JSON validation if essential keys exist

--- packages/api/index.js
+++ packages/api/index.js
@@ -3,13 +3,32 @@ const bp = require('body-parser')
 
 const app = express()
 const port = 8000
-
 app.use(bp.json())
 app.use(bp.urlencoded({extended: true}))
 
+validateBody = (obj) => {
+
+    if(('fullName' in obj && obj['fullName'] != null) && ('email' in obj && obj["email"] != null))
+    {
+        return true
+    }
+       
+    return false
+    
+}
+
+parseBody = (obj) => {
+    console.log(obj)
+}
+
 app.post('/api/signup', (req, res) => {
-    console.log(req.body)
-    res.send('Hello World!')
+
+    if(!validateBody(req.body)){
+        res.send(400)
+     }
+     else{
+        console.log(validateBody(req.body))
+     }
 })
 
 app.listen(port, () => {
