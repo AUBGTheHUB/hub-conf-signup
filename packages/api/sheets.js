4a43e32d51aa8b6a8e0ac167f0ea141223c0b724
SamuilMladenov <samuil.v.mladenov@gmail.com>
2022-10-22 14:43:56 +0300

    Add full functionality for Sheets

--- packages/api/sheets.js
+++ packages/api/sheets.js
@@ -1,6 +1,7 @@
 const { GoogleSpreadsheet } = require("google-spreadsheet");
 credentials = require("./hubconf.json")
 
+
 module.exports = {
     handleAppending: async function (obj) {
         let spreadsheet = new GoogleSpreadsheet("1N7giS-ff6At6gKO5K30WC0j0Bw-c5C5fAC4uhtZqozY")
@@ -9,8 +10,18 @@ module.exports = {
             await spreadsheet.useServiceAccountAuth(credentials)
             await spreadsheet.loadInfo()
             
-            console.log("TITLE: " + spreadsheet.title + " , BODY: " + obj.name)
+            const sheet = spreadsheet.sheetsByIndex[0]
 
+            if("school" in obj){
+                const rowAppend = await sheet.addRows([
+                    { name: obj.name , email: obj.email, school: obj.school},
+                ]);
+            }
+            else{
+                const rowAppend = await sheet.addRows([
+                { name: obj.name , email: obj.email},
+            ]);  
+            }
         }  
         catch (error) {
             console.log(error)
