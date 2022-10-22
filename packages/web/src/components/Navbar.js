04faecc0a87d989140ae529107be53482b0c486f
Aleks <al.ivanov1908@gmail.com>
2022-10-22 11:53:58 +0300

    nav-bar-progress

--- packages/web/src/components/Navbar.js
+++ packages/web/src/components/Navbar.js
@@ -3,8 +3,26 @@ import "../style.css";
 
 const Navbar = () => {
     return (
-    <div className="black">
-        <img src = "whitelogo.png"/>;
+    <div className="nav-bar">
+        <div className="nav-div">
+            <div className="logo">
+                <img  src = "whitelogo.png"/>
+            </div>
+            <div className="nav-tags">
+                <a href="">HubConf</a>
+            </div>
+            <div className="nav-tags">
+                <a href="">Registration</a>
+            </div>
+            <div className="nav-tags">
+                <a href="">Companies</a>
+            </div>
+            <div className="nav-tags">
+                <a href="">Speakers</a>
+            </div>
+        </div> 
+        {/* this is the end of the navdiv */}
+        
     </div> 
     )
 }
