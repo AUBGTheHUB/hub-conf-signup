37664e52884e3199a508fbc6a9cd6671a3952abd
SamuilMladenov <samuil.v.mladenov@gmail.com>
2022-10-22 12:08:42 +0300

    Add googleapis

--- packages/api/index.js
+++ packages/api/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const bp = require('body-parser')
+const file = require('./sheets.js')
+const append = require('./sheets')
 
 const app = express()
 const port = 8000
 
 app.use(bp.json())
 app.use(bp.urlencoded({extended: true}))
+
+app.get("/", (req, res)=> {
+    append.handleAppending(req.body)
+    res.send("OK")
+})
 
 app.post('/api/signup', (req, res) => {
     console.log(req.body)
