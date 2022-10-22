d723c283fdc55b6a01064806d105edc92531bb3b
SuelaCark <suela.qarkaxhija@gmail.com>
2022-10-22 16:42:13 +0300

    speakers flexbox reday

--- packages/web/src/components/Speakers.js
+++ packages/web/src/components/Speakers.js
@@ -9,8 +9,16 @@ const Speakers = () => {
     <div className="SpeakerDesc">
         <h1 className="heading">Speakers</h1>  
         <Flippedmb/>   
+        <div class="gallery">
+      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"></img>
+      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg"></img>
+      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg"></img>
+      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/4.jpg"></img>
+      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/5.jpg"></img>
+      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/6.jpg"></img>
+        </div>
     </div>
-</div> 
+    </div>
     )
 }
 
