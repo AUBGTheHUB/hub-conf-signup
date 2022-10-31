a09169ab77b01bd91c5adf6bfbde1ae2e8de5518
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-10-31 18:56:07 +0200

    #30 add conditional rendering for navbar

--- packages/web/src/components/Navbar.js
+++ packages/web/src/components/Navbar.js
@@ -1,30 +1,48 @@
 import React from "react";
 import "../style.css";
+import { useMediaQuery } from 'react-responsive'
 
 const Navbar = () => {
-    return (
-    <div className="nav-bar">
-        <div className="nav-div top">
-            <div className="logo">
-                <img  src = "whitelogo.png" alt="Hub logo"/>
-            </div>
-            <div className="nav-tags">
-                <a href="#Hubconf">HubConf</a>
-            </div>
-            <div className="nav-tags">
-                <a href="#Speakers">Speakers</a>
-            </div>
-            <div className="nav-tags">
-                <a href="#Registration">Registration</a>
-            </div>
-            <div className="nav-tags">
-                <a href="#Representatives">Representatives</a>
-            </div>
-        </div> 
-        {/* this is the end of the navdiv */}            
-    </div> 
 
-    )
+    const isMobile = useMediaQuery({ query: '(max-width: 1200px)' })
+
+    if (isMobile) {
+        return (
+            <div>
+                <h1>HELLO</h1>
+            </div>
+        )
+    }
+
+    else {
+        return (
+            <div className="nav-bar">
+                <div className="nav-div top">
+                    <div className="logo">
+                        <img src="whitelogo.png" alt="Hub logo" />
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Hubconf">HubConf</a>
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Speakers">Speakers</a>
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Registration">Registration</a>
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Representatives">Representatives</a>
+                    </div>
+                </div>
+                {/* this is the end of the navdiv */}
+            </div>
+
+        )
+
+
+    }
+
+
 }
 
 export default Navbar;
