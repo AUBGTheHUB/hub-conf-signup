fc24834ca36b43b7878d823114fa449756353bf0
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-11-01 12:41:19 +0200

    #31 remove ham, add conditional for nothing

--- packages/web/src/components/Navbar.js
+++ packages/web/src/components/Navbar.js
@@ -1,30 +1,37 @@
+import { useMediaQuery } from "react-responsive";
 import React from "react";
 import "../style.css";
 
 const Navbar = () => {
-    return (
-    <div className="nav-bar">
-        <div className="nav-div top">
-            <div className="logo">
-                <img  src = "whitelogo.png" alt="Hub logo"/>
-            </div>
-            <div className="nav-tags">
-                <a href="#Hubconf">HubConf</a>
-            </div>
-            <div className="nav-tags">
-                <a href="#Speakers">Speakers</a>
-            </div>
-            <div className="nav-tags">
-                <a href="#Registration">Registration</a>
-            </div>
-            <div className="nav-tags">
-                <a href="#Representatives">Representatives</a>
-            </div>
-        </div> 
-        {/* this is the end of the navdiv */}            
-    </div> 
 
-    )
+    const isMobile = useMediaQuery({ maxWidth: 1200 })
+
+    if(!isMobile) {
+        return (
+            <div className="nav-bar">
+                <div className="nav-div top">
+                    <div className="logo">
+                        <img src="whitelogo.png" alt="Hub logo" />
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Hubconf">HubConf</a>
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Speakers">Speakers</a>
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Registration">Registration</a>
+                    </div>
+                    <div className="nav-tags">
+                        <a href="#Representatives">Representatives</a>
+                    </div>
+                </div>
+                {/* this is the end of the navdiv */}
+            </div>
+
+        )
+
+    }
 }
 
 export default Navbar;
