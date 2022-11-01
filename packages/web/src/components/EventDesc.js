c50370f28ecc018518aff50c79211701730cdb0e
Aleks Ivanov <114522737+Aleks1908@users.noreply.github.com>
2022-11-01 18:37:21 +0200

    #42 fix headers and paragraphs responsive (#44)

--- packages/web/src/components/EventDesc.js
+++ packages/web/src/components/EventDesc.js
@@ -12,9 +12,15 @@ const EventDesc = () => {
             <h1 className="heading" id="Hubconf">Welcome to HubConf</h1>     
         </div>
         <div className="content">
-            <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum <br></br>
-            <br></br> Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum</p>
-            
+            <p>HubConf 2022 gives you the opportunity to learn about developments in technology and business that look like they are out of this world. 
+            </p>            
+        </div>
+        <div className="content">
+            <p>Register for the event and get the chance to listen and ask questions to some of the pioneers of our society. Companies like Progress, Chaos and Uber will also join us to offer attendees exclusive insight into their workflows. 
+                </p>            
+        </div>
+        <div className="content">
+            <p>We will be awaiting you on November 12th at the American University in Bulgaria in the BAC lobby at 12:00 PM.</p>            
         </div>
     </div>  
     )
