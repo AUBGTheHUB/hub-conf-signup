b9208aec6a7aaebd3505938da46d83f675dd6fd7
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-11-09 19:54:22 +0200

    add to public dir

--- packages/web/src/components/Speakers.js
+++ packages/web/src/components/Speakers.js
@@ -3,26 +3,26 @@ import "../style.css";
 import Flippedmb from "./Flippedmb";
 
 const Speakers = () => {
-    return (
-   
-    <div className="Speaker">     
-    <div className="SpeakerDesc"> 
-    <h1 className="heading" id="Speakers">Speakers</h1>
-        <Flippedmb/>   
-        
+  return (
+
+    <div className="Speaker">
+      <div className="SpeakerDesc">
+        <h1 className="heading" id="Speakers">Speakers</h1>
+        <Flippedmb />
+
         <div className="gallery">
-      <img src="/packages/web/src/speakers/boris_angelov.png"></img>
-      <img src="/packages/web/src/speakers/dimitar_mitkov.png"></img>
-      <img src="/packages/web/src/speakers/georgi_kostadinov.png"></img>
-      <img src="/packages/web/src/speakers/georgi_mutafchiev.png"></img>
-      <img src="/packages/web/src/speakers/ina_toncheva.png"></img>
-      <img src="/packages/web/src/speakers/ivan_angelinin.png"></img>
-      <img src="/packages/web/src/speakers/nikolay_babanov.png"></img>
-      <img src="/packages/web/src/speakers/paulina_chotrova.png"></img>
+          <img src="boris_angelov.png"></img>
+          <img src="dimitar_mitkov.png"></img>
+          <img src="georgi_kostadinov.png"></img>
+          <img src="georgi_mutafchiev.png"></img>
+          <img src="ina_toncheva.png"></img>
+          <img src="ivan_angelinin.png"></img>
+          <img src="nikolay_babanov.png"></img>
+          <img src="paulina_chotrova.png"></img>
         </div>
+      </div>
     </div>
-    </div>
-    )
+  )
 }
 
 export default Speakers;
