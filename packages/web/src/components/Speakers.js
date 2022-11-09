53396ad4977831929c1f6a8553c08c0e499a565d
GameOfInches <114807906+GameOfInches@users.noreply.github.com>
2022-11-09 21:15:52 +0300

    Add speakers (#47)
    Co-authored-by: Michael Bozhilov <michaelbozhilov@gmail.com>

--- packages/web/src/components/Speakers.js
+++ packages/web/src/components/Speakers.js
@@ -3,24 +3,26 @@ import "../style.css";
 import Flippedmb from "./Flippedmb";
 
 const Speakers = () => {
-    return (
-   
-    <div className="Speaker" hidden>     
-    <div className="SpeakerDesc"> 
-    <h1 className="heading" id="Speakers">Speakers</h1>
-        <Flippedmb/>   
-        
+  return (
+
+    <div className="Speaker">
+      <div className="SpeakerDesc">
+        <h1 className="heading" id="Speakers">Speakers</h1>
+        <Flippedmb />
+
         <div className="gallery">
-      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"></img>
-      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg"></img>
-      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg"></img>
-      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/4.jpg"></img>
-      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/5.jpg"></img>
-      <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/6.jpg"></img>
+          <img src="boris_angelov.png"></img>
+          <img src="dimitar_mitkov.png"></img>
+          <img src="georgi_kostadinov.png"></img>
+          <img src="georgi_mutafchiev.png"></img>
+          <img src="ina_toncheva.png"></img>
+          <img src="ivan_angelinin.png"></img>
+          <img src="nikolay_babanov.png"></img>
+          <img src="paulina_chotrova.png"></img>
         </div>
+      </div>
     </div>
-    </div>
-    )
+  )
 }
 
 export default Speakers;
